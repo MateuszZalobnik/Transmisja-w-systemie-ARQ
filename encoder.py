69c7aca6a0306c989d81eb77058b69e49db0fa12
MateuszZalobnik <mateusz.zalobnik@hued.me>
2024-04-19 00:18:55 +0200

    add encoding methods

--- encoder.py
+++ encoder.py
@@ -1,8 +1,19 @@
 class encoder:
     @staticmethod
-    def encode_with_crc(data):
+    def encode_with_crc8(data):
         # Utwórz obiekt CRC z określonymi parametrami
         pass
+
+    @staticmethod
+    def encode_with_crc16(data):
+        # Utwórz obiekt CRC z określonymi parametrami
+        pass
+
+    @staticmethod
+    def encode_with_crc32(data):
+        # Utwórz obiekt CRC z określonymi parametrami
+        pass
+
 
     @staticmethod
     def encode_with_parity(data):
