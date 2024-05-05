f739725621c5d12703013a27d88be89fd0d848be
MateuszZalobnik <mateusz.zalobnik@hued.me>
2024-05-05 21:28:23 +0200

    add error generate methods

--- channel.py
+++ channel.py
@@ -2,11 +2,24 @@ import komm
 
 
 class channel:
-    @staticmethod
-    def generate_errors_with_bsc(data, error_probability):
-        bsc = komm.BinarySymmetricChannel(error_probability)
+    def __init__(self, error_probability):
+        self.error_probability = error_probability
+        pass
+
+    def generate_errors_with_bsc(self, data):
+        bsc = komm.BinarySymmetricChannel(self.error_probability)
         return bsc(data)
 
-    @staticmethod
-    def generate_errors_with_gilbert_elliott(data, error_probability):
+    def generate_random_errors(self, data):
+        # wprowadź błędy losowe z prawdopodobieństwem error_probability
         pass
+
+    def generate_burst_errors(self, data):
+        # wprowadź błędy skupione z prawdopodobieństwem error_probability
+        pass
+
+    def generate_impulse_errors(self, data):
+        # wprowadź błędy impulsowe z prawdopodobieństwem error_probability
+        pass
+
+
